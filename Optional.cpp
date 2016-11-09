8872fcb4d17044468bfe3d5815457109158541ac
akash kandpal <cool_akashkandpal@rediffmail.com>
2016-11-10 03:03:43 +0530

    Create Optional.cpp

---
+++ Optional.cpp
@@ -0,0 +1,16 @@
+Optional include list
+
+Use it when there is no bits/stdc++.h
+#include <iostream>
+#include <cstring>
+#include <cmath>
+#include <algorithm>
+#include <climits>
+#include <stack>
+#include <queue>
+#include <vector>
+#include <set>
+#include <map>
+#include <list>
+#include <cassert>
+#include <unordered_map>
